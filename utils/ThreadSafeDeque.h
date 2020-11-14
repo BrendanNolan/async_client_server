8c74d390fa196a376e8279affcda23f0550eba69
BrendanNolan <bnolan23@gmail.com>
2020-11-14 14:31:47 +0000

    Updated ThreadSafeDeque

--- utils/ThreadSafeDeque.h
+++ utils/ThreadSafeDeque.h
@@ -2,7 +2,9 @@
 #define THREADSAFEDEQUE_H
 
 #include <deque>
+#include <mutex>
 #include <optional>
+#include <utility>
 
 template <typename T>
 class ThreadSafeDeuqeue
@@ -10,11 +12,16 @@ class ThreadSafeDeuqeue
 public:
     std::optional<T> pop()
     {
-
+        std::scoped_lock lock(mutex_);
+        if (deque_.empty())
+            return {};
+        auto ret = std::move(deque_.back());
+        return {ret};
     }
 
 private:
     std::deque<T> deque_;
+    std::mutex mutex_;
 };
 
 #endif// THREADSAFEDEQUE_H
