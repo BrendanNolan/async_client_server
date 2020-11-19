62da5bd0a6487c2c928e3574116d22dbe6e91faf
brendan_m_nolan <bnolan23@gmail.com>
2020-11-19 20:59:13 +0000

    Some small progress on Client.

--- client/Client.cpp
+++ client/Client.cpp
@@ -28,49 +28,90 @@ void log(const std::string& text, const std::string& /*logFile*/)
 
 Client::Client(io_context& iocontext)
     : iocontext_{ &iocontext }
+    , socket_{ iocontext }
 {
 }
 
 void Client::start()
 {
-    tcp::resolver::query q{ "192.168.1.12", "2014" };
     tcp::resolver resolver{ *iocontext_ };
-    auto it = resolver.resolve(q);
+    resolver.async_resolve(
+        "192.168.1.12",
+        "2014",
+        [this](
+            const boost::system::error_code& error,
+            boost::asio::ip::tcp::resolver::results_type results) {
+            handleResolve(error, results);
+        });
 }
 
-void Client::resolveHandler(
-    const boost::system::error_code& ec,
-    boost::asio::ip::tcp::resolver::iterator it)
-{
-}
-
-void Client::connectHandler(const boost::system::error_code& ec)
-{
-}
-
-void Client::readHandler(
+void Client::handleRead(
     const boost::system::error_code& ec, std::size_t bytes_transferred)
 {
+    std::cout << "Received from server: " << 
+        std::string(
+            messageFromServer_.begin(), 
+            messageFromServer_.begin() + bytes_transferred);
 }
 
-void Client::handleConnection(tcp::socket& socket)
+void Client::handleResolve(
+    const boost::system::error_code& error,
+    boost::asio::ip::tcp::resolver::results_type results)
 {
-    std::array<char, 4096> bytes;
-    auto moreToRead = true;
-    while (moreToRead)
-    {
-        try
-        {
-            socket.read_some(buffer(bytes));
-            log("Socket read succeeded.\n", logFile_);
-        }
-        catch (const boost::system::system_error& error)
-        {
-            log(std::string("Socket read_some failed with error message: ")
-                    + std::string(error.what()) + '\n',
-                logFile_);
-            moreToRead = false;
-        }
-    }
-    log(bytes.data(), logFile_);
+    boost::asio::async_connect(
+        socket_,
+        results,
+        [this](
+            const boost::system::error_code& error,
+            const boost::asio::ip::tcp::endpoint& endpoint) {
+            handleConnection(error, endpoint);
+        });
 }
+
+void Client::handleConnection(
+    const boost::system::error_code& error,
+    const boost::asio::ip::tcp::endpoint& endpoint)
+{
+    messageForServer_ = "Hello, I am the client.\n";
+    async_write(
+        socket,
+        buffer(messageForServer_),
+        [this](
+            const boost::system::error_code& error,
+            std::size_t bytes_transferred) { 
+            handleWrite(error, bytes_transferred);
+        });
+}
+
+void Client::handleWrite(
+    const boost::system::error_code& error, std::size_t bytes_transferred)
+{
+    async_read(
+        socket, 
+        buffer(messageFromServer_),
+        [this](
+            const boost::system::error_code& ec, 
+            std::size_t bytes_transferred) { 
+            handleRead(ec, bytes_transferred);
+        });
+}
+
+//void Client::writeHandler()
+//{
+//    while (moreToRead)
+//    {
+//        try
+//        {
+//            socket.read_some(buffer(bytes));
+//            log("Socket read succeeded.\n", logFile_);
+//        }
+//        catch (const boost::system::system_error& error)
+//        {
+//            log(std::string("Socket read_some failed with error message: ")
+//                    + std::string(error.what()) + '\n',
+//                logFile_);
+//            moreToRead = false;
+//        }
+//    }
+//    log(bytes.data(), logFile_);
+//}
--- client/Client.h
+++ client/Client.h
@@ -12,18 +12,26 @@ public:
     void start();
 
 private:
-    void resolveHandler(
-        const boost::system::error_code& ec,
-        boost::asio::ip::tcp::resolver::iterator it);
-    void connectHandler(const boost::system::error_code& ec);
-    void readHandler(
+    void handleConnection(const boost::system::error_code& ec);
+    void handleRead(
         const boost::system::error_code& ec, std::size_t bytes_transferred);
+    void handleResolve(
+        const boost::system::error_code& error, 
+        boost::asio::ip::tcp::resolver::results_type results);
 
-    void handleConnection(boost::asio::ip::tcp::socket& socket);
+    void handleConnection(
+        const boost::system::error_code& error, 
+        const boost::asio::ip::tcp::endpoint& endpoint);
+
+    void handleWrite(
+        const boost::system::error_code& error,
+        std::size_t bytes_transferred);
 
 private:
     boost::asio::io_context* iocontext_ = nullptr;
-    std::string logFile_ = "web_client.log";
+    boost::asio::ip::tcp::socket socket_;
+    std::string messageForServer_;
+    std::array<char, 256> messageFromServer_;
 };
 
 #endif// CLIENT_H
