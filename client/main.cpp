d3150892b64a344f3a09d93faed873ce06368c3b
BrendanNolan <bnolan23@gmail.com>
2020-12-22 20:15:14 +0000

    Tweaks to client/main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <thread>
 #include <utility>
@@ -7,36 +8,34 @@
 #include "Client.h"
 #include "Message.h"
 
+void waitForConnection(const Client& client)
+{
+    if (!client.connectionEstablished())
+        std::cout << "Connecting...";
+    while (!client.connectionEstablished())
+    {
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(0.01s);
+        std::cout << '.';
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
     boost::asio::io_context iocontext;
+    
     Client client(iocontext);
     client.start();
 
     std::thread thread{ [&iocontext]() { iocontext.run(); } };
 
-    while (!client.connectionEstablished())
-    {
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(1s);
-    }
+    waitForConnection(client);
 
-    std::vector<std::string> messages = { 
-        "Dirty", 
-        "old",     
-        "river",
-        ",",     
-        "must",    
-        "you",
-        "keep",  
-        "rolling", 
-        "rolling",
-        "on" };
-
-    for (const auto& msg : messages)
+    for (auto i = 0; i < 10; ++i)
     {
         utils::Message message;
-        message << msg;
+        message << i;
         client.send(std::move(message));
     }
 
