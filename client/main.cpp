fda223ee24a99e3c4ce0bd9edfc6d70943511a00
BrendanNolan <bnolan23@gmail.com>
2021-01-01 14:47:55 +0000

    client program now takes number of simulated clients as a command-line argument.

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <thread>
@@ -11,28 +12,30 @@
 #include "PrintLogger.h"
 #include "Message.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 2)
+        return -1;
+
     boost::asio::io_context iocontext;
 
     auto printLogger = std::make_shared<utils::PrintLogger>();
 
     std::vector<std::thread> threads;
-
-    for (auto i = 0; i < 20; ++i)
+    
+    const auto threadCount = std::atoi(argv[1]);
+    for (auto i = 0; i < threadCount; ++i)
     {
-        threads.push_back(
-            std::thread([&iocontext, printLogger]() {
-                Client client(iocontext, std::move(printLogger));
-                client.start();        
-                for (auto i = 0; i < 10000000; ++i)
-                {
-                    utils::Message message;
-                    message << i;
-                    client.send(std::move(message));
-                }
-            }));
-        
+        threads.push_back(std::thread([&iocontext, printLogger]() {
+            Client client(iocontext, std::move(printLogger));
+            client.start();
+            for (auto i = 0; i < 10000000; ++i)
+            {
+                utils::Message message;
+                message << i;
+                client.send(std::move(message));
+            }
+        }));
     }
 
     std::thread thread{ [&iocontext]() { iocontext.run(); } };
