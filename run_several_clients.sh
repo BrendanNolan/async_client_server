ec5e4bea96e16fc395bec53e1185d7bb6d0f4236
BrendanNolan <bnolan23@gmail.com>
2021-11-16 20:59:40 +0000

    Update run_several_clients.sh
    Now works with debug and release builds.

--- run_several_clients.sh
+++ run_several_clients.sh
@@ -1,5 +1,14 @@
-for i in $(seq 1 $1);
+build_type=$1
+client_count=$2
+
+if [ ${build_type} != "Debug" ] && [ ${build_type} != "Release" ];
+then
+    echo "Argument 1 bust be \"Debug\" or \"Release\""
+    exit 1
+fi
+
+for i in $(seq 1 ${client_count});
 do
-    ./build/client/client &
+    ./build/${build_type}/client/client &
 done
 
