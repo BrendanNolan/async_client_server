9a8f294bd30238fa2fe9127ff72a2528fae345e4
BrendanNolan <bnolan23@gmail.com>
2021-12-07 18:48:03 +0000

    Attempting to use conan

---
+++ conanfile.py
@@ -0,0 +1,12 @@
+from conans import ConanFile, CMake
+
+class async_client_server(ConanFile):
+    name = "async_client_server"
+    requires = "boost/1.73.0"
+    generators = "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
